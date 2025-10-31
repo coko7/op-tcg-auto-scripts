23b94bbaeb865747a1e3c043ce1ffc5571ba01e3
Coko <91132775+Coko7@users.noreply.github.com>
2025-10-31 12:38:30 +0100

    fix: refactor/improve open-booster.sh by using for loop instead of sed

--- open-booster.sh
+++ open-booster.sh
@@ -10,13 +10,30 @@ if [ -z "$PACK_ID" ]; then
     exit 1
 fi
 
-echo -e "${FG_RED}<<<===<<<ooo<<<[[{0}]]>>>ooo>>>===>>>${COL_RESET}"
+function get_pretty_rarity_text() {
+    rarity="$1"
+    case $rarity in
+        'common') echo "${FG_GRAY}Common${COL_RESET}" ;;
+        'uncommon') echo "${FG_GREEN}Uncommon${COL_RESET}" ;;
+        'rare') echo "${FG_BLUE}Rare${COL_RESET}" ;;
+        'leader') echo "${FG_RED}Leader 👑${COL_RESET}" ;;
+        'super_rare') echo "${FG_CYAN}⭐ Super Rare ⭐${COL_RESET}" ;;
+        'secret_rare') echo "${FG_YELLOW}🌟🌈✨ Secret Rare ✨🌈🌟${COL_RESET}" ;;
+        *) echo "${FG_RED}❓ ??? ❓${COL_RESET}" ;;
+    esac
+}
+
+function print_separator() {
+    echo -e "${FG_RED}<<<===<<<ooo<<<[[{0}]]>>>ooo>>>===>>>${COL_RESET}"
+}
 
 res=$(curl 'https://backend-optcg.polo2409.work/api/users/buy-booster' \
   --compressed --silent -X POST \
   -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $BEARER" \
   --data-raw '{"boosterId":"'"$PACK_ID"'"}')
+
+print_separator
 
 if echo "$res" | jq -e 'has("error")' > /dev/null; then
     error=$(echo "$res" | jq -r '.error')
@@ -25,14 +42,13 @@ if echo "$res" | jq -e 'has("error")' > /dev/null; then
     exit 1
 fi
 
-res=$(echo "$res" | jq -r '.data.cards.[] | "\(.name): \(.rarity) (\(.vegapull_id))"')
-res=$(echo "$res" \
-    | sed "s| leader| \\${FG_RED}Leader\\${COL_RESET}|g" \
-    | sed "s| uncommon| \\${FG_GREEN}Uncommon\\${COL_RESET}|g" \
-    | sed "s| secret_rare| \\${FG_YELLOW}✨ SECRET Rare ✨\\${COL_RESET}|g" \
-    | sed "s| super_rare| \\${FG_PURPLE}Super Rare\\${COL_RESET}|g" \
-    | sed "s| common| \\${FG_GRAY}Common\\${COL_RESET}|g" \
-    | sed "s| rare| \\${FG_BLUE}Rare\\${COL_RESET}|g")
+echo "$res" | jq -c '.data.cards.[]' | while read -r card; do
+    id=$(echo "$card" | jq -r '.vegapull_id')
+    name=$(echo "$card" | jq -r '.name')
+    rarity=$(echo "$card" | jq -r '.rarity')
 
-echo -e "$res"
-echo -e "${FG_RED}<<<===<<<ooo<<<[[{0}]]>>>ooo>>>===>>>${COL_RESET}"
+    pretty_rarity=$(get_pretty_rarity_text "$rarity")
+    echo -e "$pretty_rarity: $name (${FG_GRAY}$id${COL_RESET})"
+done
+
+print_separator
